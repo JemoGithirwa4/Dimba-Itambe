4f4526019e746cebe9202f975a9548ab27caa264
James Wainaina Githirwa <jagithirwa@gmail.com>
2025-01-10 13:37:28 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { activePage: "latest" });
+});
+
+app.get("/watch", (req, res) => {
+    res.render("watch.ejs", { activePage: "watch" });
+});
+
+app.get("/teams", (req, res) => {
+    res.render("teams.ejs", { activePage: "teams" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
