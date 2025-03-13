9fd11161d5368f64bee96cc8e745a3969748646b
James Wainaina Githirwa <jagithirwa@gmail.com>
2025-03-13 21:26:36 +0300

    Blog Page UI setup

--- app.js
+++ app.js
@@ -1,15 +1,63 @@
 import express from "express";
 import bodyParser from "body-parser";
+import env from "dotenv";
+import pg from "pg";
 
 const PORT = process.env.PORT || 3005;
 
 const app = express();
 
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+  
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { activePage: "latest" });
+let articles = [];
+let videos = [];
+let teams = [];
+let players = [];
+let stats = [];
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await db.query(
+            "SELECT * FROM Articles ORDER BY publisheddate DESC"
+        );
+        articles = result.rows;
+
+        res.render("index.ejs", { activePage: "latest", articles: articles });
+    } catch (err){
+        console.log(err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.get("/article/:id", async(req, res) => {
+    try {
+        const articleId = req.params.id;
+        const result = await db.query(
+            "SELECT * FROM Articles WHERE articleid = $1", [articleId]
+        );
+
+        let blog = result.rows;
+
+        res.render("home/article.ejs", {
+            activePage: "latest",
+            article: blog[0]
+        });
+    } catch (error) {
+      res.status(500).send("Error fetching post");
+    }
 });
 
 app.get("/pay-ticket", (req, res) => {
